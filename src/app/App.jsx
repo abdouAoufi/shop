829d177a6d95b83b35e053d2ae824421d41ca261
Aoufi Abderahmane <abdouou7@gmail.com>
2022-01-28 15:43:52 +0100

    add btn

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar/Navbar";
+import Cart from "../components/Cart/Cart";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Cart/>
     </div>
   );
 }
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,13 +1,10 @@
 import iphone from "../../images/iphone.jpeg";
-import "./root.css"
+import "./root.css";
 
 function Cart() {
   return (
-    <div>
-      <img
-        src={iphone}
-        alt=""
-      />
+    <div className="product">
+      <img src={iphone} alt="" />
       <div>
         <h3>Product title</h3>
         <p>1200$</p>
@@ -16,6 +13,8 @@ function Cart() {
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
         impedit maiores modi vel.
       </p>
+      <button>Add to cart</button>
     </div>
   );
 }
+export default Cart;
