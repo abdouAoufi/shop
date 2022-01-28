9944867e81b4f83361bf20e4aac9d413ac55d85a
Aoufi Abderahmane <abdouou7@gmail.com>
2022-01-28 16:10:23 +0100

    add data.json

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,22 @@
 import Navbar from "../components/Navbar/Navbar";
 import Cart from "../components/Cart/Cart";
+import data from "../constants/data.json"
 
 function App() {
+  console.log(data.list)
   return (
     <div>
       <Navbar />
-      <Cart/>
+      <div style={{ display: "flex", flexWrap: "wrap" , gap : 12 }}>
+        <Cart />
+        <Cart />
+        <Cart />
+        <Cart />
+        <Cart />
+        <Cart />
+        <Cart />
+        <Cart />
+      </div>
     </div>
   );
 }
